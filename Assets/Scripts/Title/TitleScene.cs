7320b83b9576db3d3ab67c2e9a3a39643d168d2e
Raycast <hyeok0215@naver.com>
2023-10-18 00:16:39 +0900

    [Modified] 타이틀 씬 클래스 추가

--- Assets/Scripts/Title/TitleScene.cs
+++ Assets/Scripts/Title/TitleScene.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using static Common.EnumClass;
 
 namespace Title
 {
@@ -11,15 +12,18 @@ namespace Title
         private const float OutAlpha = 0.2f;
         private const float Duration = 1.0f;
         private const float FastSpeedOffset = 0.9f;
-        
+
         [SerializeField] private Text touchText;
 
         private float _speed;
         private bool _isFadeOut;
         private Button _touchButton;
         private Coroutine _fadeRoutine;
+        private Coroutine _moveSceneRoutine;
         private Color _color = Color.white;
 
+        private bool _isSelected;
+        
         private void Awake()
         {
             _touchButton = touchText.GetComponent<Button>();
@@ -31,30 +35,51 @@ namespace Title
 
         private void Start()
         {
-            Clear();
-            _fadeRoutine = StartCoroutine(OnFade());
+            //DOTween.KillAll();
+            
+            StartFade();
         }
 
         private IEnumerator OnFade()
         {
             while (true)
             {
-                _color.a = OutAlpha;
-                touchText.DOColor(_color, Duration - _speed).OnComplete(() => _isFadeOut = false);;
+                Fade(true);
                 
                 yield return new WaitUntil(() => _isFadeOut is false);
 
-                _color.a = 1.0f;
-                touchText.DOColor(_color, Duration - _speed).OnComplete(() => _isFadeOut = true);
+                Fade(false);
                 
                 yield return new WaitUntil(() => _isFadeOut);
             }
         }
 
+        private void Fade(bool isFade)
+        {
+            _color.a = isFade ? OutAlpha : 1.0f;
+            touchText.DOColor(_color, Duration - _speed).OnComplete(() => _isFadeOut = !isFade);;
+        }
+        
         private void MoveToLobby()
         {
-            _speed = FastSpeedOffset;
+            if(_isSelected) return;
             
+            _isSelected = true;
+            
+            _speed = FastSpeedOffset;
+
+            StartFade();
+            StartCoroutine(MoveScene());
+            
+            IEnumerator MoveScene()
+            {
+                yield return new WaitForSeconds(1.0f);
+                GameManager.Instance.SceneLoader.LoadScene(SceneName.Lobby);
+            }
+        }
+        
+        private void StartFade()
+        {
             Clear();
             _fadeRoutine = StartCoroutine(OnFade());
         }
